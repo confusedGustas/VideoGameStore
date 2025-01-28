fbb6ea02fa4dd103ec47ff5d25c6f3a77ce80be4
confusedGustas <gustas.antanas2003@gmail.com>
2025-01-28 17:30:21 +0200

    removed unused dependencies

--- src/main/java/com/gustas/videogamestore/VideoGameStoreApplication.java
+++ src/main/java/com/gustas/videogamestore/VideoGameStoreApplication.java
@@ -2,10 +2,8 @@ package com.gustas.videogamestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class VideoGameStoreApplication {
 
     public static void main(String[] args) {
